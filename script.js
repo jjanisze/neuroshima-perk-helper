cfffa2a10e916f6a8b5f73f80bf2f44eb6ba3543
Jacek Janiszewski <jacek.janiszewski@intel.com>
2023-10-22 14:02:29 +0200

    Add basic stat fields

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const urls = [
     'perk.json',
 ];
 
+// Global list of rulesets
+var rulesets = [];
+
 // Global ID to Identifiable lookup table
 var IDLookup = {};
 
@@ -16,6 +19,9 @@ class Identifiable {
         
         // Validate ID
         var IDstr = String(this.ID).trim().toUpperCase();
+        if( IDstr.length < 3 ) {
+            throw new Error("IDs cannot be shorter than 3 characters");
+        }
         if( IDstr != this.ID ) {
             throw new Error("Invalid ID '"+this.Name+"'");
         }
@@ -23,9 +29,22 @@ class Identifiable {
             throw new Error("Duplicate ID '%s' found", IDstr);
         }
         IDLookup[this.ID] = this;
+
+        // Validate Ruleset
+        var Rulesetstr = String(this.Ruleset).trim();
+        if( Rulesetstr.length < 3 ) {
+            throw new Error("Ruleset names can't be shorter than 4 for "+this.ID);
+        }
+        if( Rulesetstr != this.Ruleset ) {
+            throw new Error("Extra whitespace in Ruleset name for "+this.ID);
+        }
+        if( ! rulesets.includes(this.Ruleset) ) {
+            rulesets.push(this.Ruleset);
+        }
     }
     get ID() { return this.data.ID; }
     get Name() { return this.data.Name; }
+    get Ruleset() { return this.data.Ruleset; }
 }
 
 class Describable extends Identifiable {
@@ -53,6 +72,15 @@ function parseAttributes(data) {
     });
     console.log("Parsed %d attributes", attributes.length);
 }
+function constructAttributeHTML() {
+    for (var i=0; i<attributes.length; ++i) {
+        var atr = attributes[i];
+        var html = '<div class="attribute-instance"><h3>';
+        html += atr.Name;
+        html += '</h3><input type="number" id="'+atr.ID+'" min="0" max="30" /></div>' ;
+        $('#attributes-container').append(html);
+    }
+}
 
 
 // Specializations
@@ -71,9 +99,17 @@ function parseSpecializations(data) {
     });
     console.log("Parsed %d specializations", attributes.length);
 }
+function constructSpecializationHTML() {
+    for (var i=0; i<specializations.length; ++i) {
+        var spe = specializations[i];
+        var html = '<option value="'+spe.ID+'">'+spe.Name+'</option>';
+        $('#specialization-selector').append(html);
+    }
+}
 
 // Skills
 var skills = [];
+var skillsTree = {}; // {ATR_ID:{GROUPNAME:[skill,skill]}
 class Skill extends Describable {
     constructor(data) {
         super(data);
@@ -102,16 +138,66 @@ class Skill extends Describable {
         console.log("Added Skill %s", this.Name);
     }
     get Attribute() { return this.attribute; }
-    get Specialization() { return this.specialization; }
+    get Specialization() { return this.data.specialization; }
+    get GroupName() { return this.data.GroupName; }
 }
 function parseSkills(data) {
     Object.keys(data).forEach(function(key){
         var item = data[key];
         var skill = new Skill(item);
         skills.push(skill);
+
+        // Create skills tree
+        if( ! (skill.Attribute.ID in skillsTree)) {
+            skillsTree[skill.Attribute.ID] = {};
+        }
+        skillgroups = skillsTree[skill.Attribute.ID];
+        if( ! (skill.GroupName in skillgroups)) {
+            skillgroups[skill.GroupName] = [];
+        }
+        skillgroups[skill.GroupName].push(skill);
     });
     console.log("Parsed %d skills", skills.length);
 }
+function constructSkillHTML() {
+    html = "";
+    for (const[attr, grpdict] of Object.entries(skillsTree)) {
+        html += '<div class="skill-attribute-group">';
+        html += '<h2>' + IDLookup[attr].Name + '</h2>';
+        for (const[grpname, grparr] of Object.entries(grpdict)) {
+            html += '<div class="skill-sub-group">';
+            html += '<h3>' + grpname + '</h3>';
+            for(let i=0; i<grparr.length; ++i) {
+                let skl = grparr[i];
+                html += '<div class="skill-item"><h4>'+skl.Name+'</h4>'
+                html += '<input type="number" id="'+skl.ID+'" min="0" max="30"/></div>'
+                
+            }
+            html += '</div>';
+        }
+        html += '</div>';
+    }
+    $('#skills-container').append(html);
+}
+
+// Classes
+var classes = [];
+class Class extends Describable {
+    constructor(data) {
+        super(data);
+        console.log("Added Class %s", this.Name);
+    }
+    get FlavorText() { return this.data["Flavor text"]; }
+}
+function parseClasses(data) {
+    Object.keys(data).forEach(function(key){
+        var item = data[key];
+        var cls = new Class(item);
+        classes.push(cls);
+    });
+    console.log("Parsed %d classes", classes.length);
+}
+
 
 class PerkRequirement {
     constructor(text) {
@@ -119,8 +205,20 @@ class PerkRequirement {
     }
 }
 
-class Perk {
-    constructor(name) {}
+var perks = [];
+class Perk extends Describable {
+    constructor(data) {
+        super(data);
+    }
+}
+
+function parsePerks(data) {
+    Object.keys(data).forEach(function(key){
+        var item = data[key];
+        var perk = new Perk(item);
+        perks.push(perk);
+    });
+    console.log("Parsed %d perks", perks.length);
 }
 
 /*
@@ -150,6 +248,13 @@ $(document).ready(function() {
         parseAttributes(JSON.parse(data[0]));
         parseSpecializations(JSON.parse(data[1]));
         parseSkills(JSON.parse(data[2]));
+        parseClasses(JSON.parse(data[3]));
+        parsePerks(JSON.parse(data[4]));
+
+        // Build HTML elements
+        constructAttributeHTML();
+        constructSpecializationHTML();
+        constructSkillHTML();
     })
     .catch(error => {
         console.error('An error occurred:', error);
